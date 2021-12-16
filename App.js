20efdd3077fca4a7156e046384ca64aab0f08ba7
Nabeel Salam <nabeelsalam00@gmail.com>
2021-12-16 00:50:44 -0500

    Initial commit

--- App.js
+++ App.js
@@ -1,21 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  TouchableOpacity,
+  View,
+  Image,
+  Button,
+  Platform,
+} from "react-native";
+import { useDeviceOrientation } from "@react-native-community/hooks";
 
 export default function App() {
+  console.log("starting app");
+  console.log(useDeviceOrientation());
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View>
+        <TouchableOpacity
+          onPress={() => {
+            console.log("pressed");
+          }}
+          onLongPress={() => {
+            console.log("long press");
+          }}
+        >
+          <Text>Hello to this demo</Text>
+        </TouchableOpacity>
+      </View>
+      <View>
+        <Button
+          title="Click Me"
+          onPress={() => {
+            console.log("button pressed");
+            alert("Button Clicked");
+          }}
+        ></Button>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "red",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
 });
---
+++ README.md
@@ -0,0 +1,3 @@
+# React Native Tutorial - following Mosh Hamedani's youtube crash course
+
+This project was bootstrapped with Expo cli
